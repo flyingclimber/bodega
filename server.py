dcf8fab7c19dcfbf6c594d9808a0ee5e5dd348f1
Tomasz Finc <tomasz@gmail.com>
2015-12-08 00:41:03 -0800

    Adding fuzzy matching

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+from fuzzywuzzy import fuzz
 
 PARSER = argparse.ArgumentParser(description='category lookup')
 PARSER.add_argument('merchant', type=str, help='merchant to lookup')
@@ -17,4 +18,9 @@ with open(INDEX) as f:
 
     if MERCHANTS.has_key(MERCHANT[:LENGTH]):
         print MERCHANTS[MERCHANT[:LENGTH]]
+    else:
+        keys = MERCHANTS.keys()
+        for key in keys:
+            if fuzz.partial_ratio(ARGS.merchant, key) > 70:
+                print 'FZ', MERCHANTS[key]
 
