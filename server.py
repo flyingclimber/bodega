3a78adccd0616e04c8061a527d5b3164f5a69cda
Tomasz Finc <tomasz@gmail.com>
2015-12-13 00:41:50 -0800

    Adding API keys

--- server.py
+++ server.py
@@ -7,10 +7,17 @@ server.py - Mini flask API
 import json
 from fuzzywuzzy import fuzz
 from flask import Flask, request
+from ConfigParser import ConfigParser
 
 APP = Flask(__name__)
 INDEX = 'index.json'
+CONFIGFILE = '.config'
 MERCHANTS = {}
+API_KEYS = []
+
+config = ConfigParser()
+config.readfp(open(CONFIGFILE))
+API_KEYS.append(config.get('API_KEYS', 'KEYS'))
 
 with open(INDEX) as f:
     MERCHANTS = json.load(f)
@@ -19,6 +26,13 @@ with open(INDEX) as f:
 def save_index():
     with open(INDEX, 'w') as g:
         g.write(json.dumps(MERCHANTS))
+
+
+def valid_api_key(key):
+    if key in API_KEYS:
+        return True
+    else:
+        return False
 
 
 @APP.route("/get/<merchant>")
@@ -34,22 +48,29 @@ def get_merchant(merchant):
                 res = MERCHANTS[key]
         return res
 
+
 @APP.route("/add", methods=['POST'])
 def add_category():
 
     merchant = request.form['merchant'].lower()
     category = request.form['category']
+    api_key = request.form['api_key']
 
-    if merchant in MERCHANTS:
-        return 'Already Present'
+    if not api_key:
+        return 'NO API KEY'
+    elif not valid_api_key(api_key):
+        return 'INVALID API KEY'
     else:
-        keys = MERCHANTS.keys()
-        for key in keys:
-            if fuzz.partial_ratio(merchant, key) > 70:
-                return 'FZ Already Present'
-        MERCHANTS[merchant] = category
-        save_index()
-        return 'Added'
+        if merchant in MERCHANTS:
+            return 'Already Present'
+        else:
+            keys = MERCHANTS.keys()
+            for key in keys:
+                if fuzz.partial_ratio(merchant, key) > 70:
+                    return 'FZ Already Present'
+            MERCHANTS[merchant] = category
+            save_index()
+            return 'Added'
 
 if __name__ == "__main__":
     APP.run(port=8080, debug=True)
