656acba1438dabd44c1aa226aa28a054794f8063
Tomasz Finc <tomasz@gmail.com>
2015-12-13 00:14:50 -0800

    Adding /add for training

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ server.py - Mini flask API
 
 import json
 from fuzzywuzzy import fuzz
-from flask import Flask
+from flask import Flask, request
 
 APP = Flask(__name__)
 INDEX = 'index.json'
@@ -14,6 +14,11 @@ MERCHANTS = {}
 
 with open(INDEX) as f:
     MERCHANTS = json.load(f)
+
+
+def save_index():
+    with open(INDEX, 'w') as g:
+        g.write(json.dumps(MERCHANTS))
 
 
 @APP.route("/get/<merchant>")
@@ -28,6 +33,23 @@ def get_merchant(merchant):
             if fuzz.partial_ratio(merchant, key) > 70:
                 res = MERCHANTS[key]
         return res
+
+@APP.route("/add", methods=['POST'])
+def add_category():
+
+    merchant = request.form['merchant'].lower()
+    category = request.form['category']
+
+    if merchant in MERCHANTS:
+        return 'Already Present'
+    else:
+        keys = MERCHANTS.keys()
+        for key in keys:
+            if fuzz.partial_ratio(merchant, key) > 70:
+                return 'FZ Already Present'
+        MERCHANTS[merchant] = category
+        save_index()
+        return 'Added'
 
 if __name__ == "__main__":
     APP.run(port=8080, debug=True)
