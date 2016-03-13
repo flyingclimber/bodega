2d4f7f7c173843c76b6b60c639ddb8402a2e538d
Tomasz Finc <tomasz@gmail.com>
2016-03-13 16:54:17 -0700

    Adopting new class in gen_index

--- gen_index.py
+++ gen_index.py
@@ -4,27 +4,23 @@
 gen_index - Generate a bodega index
 '''
 
-import json
-import os
 import argparse
+from server import Bodega, Merchant
 
 PARSER = argparse.ArgumentParser(description='index generator')
 PARSER.add_argument('file', type=str, help='file to read')
 
 ARGS = PARSER.parse_args()
-
 SOURCE = ARGS.file
-INDEX = 'index.json'
-MERCHANTS = {}
 
-if os.path.isfile(INDEX):
-    with open(INDEX) as h:
-        MERCHANTS = json.load(h)
 
-with open(SOURCE) as f:
-    for line in f:
-        item, category = line.rstrip().split(",")
-        MERCHANTS[item.lower()] = category
+if __name__ == "__main__":
+    bodega = Bodega()
 
-with open(INDEX, 'w') as g:
-    g.write(json.dumps(MERCHANTS))
+    with open(SOURCE) as f:
+        for line in f:
+            item, category = line.rstrip().split(",")
+            merchant = Merchant(item)
+            bodega.index.addmerchant(merchant, category)
+
+    bodega.saveindex()
