93cdd48969bdb975409e440ed153020a9e442cfb
Tomasz Finc <tomasz@gmail.com>
2016-04-01 21:22:04 -0700

    Adding doc strings

--- bodega.py
+++ bodega.py
@@ -24,6 +24,9 @@ class Bodega:
         self.load_index()
 
     def load_index(self):
+        """
+            Create an index if it doesn't exists or load it from a file
+        """
         if not os.path.isfile(self.json_index):
             self.save_index()
 
@@ -31,10 +34,16 @@ class Bodega:
             self.index = MerchantIndex(json.load(f))
 
     def save_index(self):
+        """
+            Write out the current index to file
+        """
         with open(self.json_index, 'w') as g:
             g.write(json.dumps(self.index.get_merchants()))
 
     def valid_api_key(self, key):
+        """
+            Check if an API key is valid
+        """
         try:
             res = self.api_keys[key]
         except KeyError:
@@ -59,6 +68,9 @@ class MerchantIndex:
         self.merchants = items or []
 
     def get_merchant_category(self, merchant):
+        """
+            Given a merchant return its category
+        """
         try:
             res = self.merchants[merchant.name]
         except KeyError:
@@ -66,12 +78,21 @@ class MerchantIndex:
         return res
 
     def get_merchants(self):
+        """
+            Return all the merchants
+        """
         return self.merchants
 
     def add_merchant(self, merchant, category=None):
+        """
+            Add a new merchant
+        """
         self.merchants[merchant.name] = category
 
     def search(self, term):
+        """
+            Given a merchant, see if it's in the index
+        """
         keys = self.merchants.keys()
         res = ''
 
@@ -83,6 +104,9 @@ class MerchantIndex:
 
 @APP.route("/get/<name>")
 def get_merchant(name):
+    """
+        Given a merchant name return if we know about it
+    """
     merchant = Merchant(name.lower())
     category = bodega.index.get_merchant_category(merchant)
 
@@ -96,6 +120,9 @@ def get_merchant(name):
 
 @APP.route("/add", methods=['POST'])
 def add_category():
+    """
+        Add a new merchant
+    """
     merchant_name = request.form['merchant'].lower()
     category = request.form['category']
     api_key = request.form['api_key']
@@ -119,6 +146,9 @@ def add_category():
 
 
 def format_output(res):
+    """
+        Given a string return JSON formatted output
+    """
     return jsonify(results=res)
 
 if __name__ == "__main__":
